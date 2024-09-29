4a52b7de21743ac60761ec5a4b8017ba23b2282c
Md Sharique Anwer <shariqueanwer002@gmail.com>
2024-09-30 01:44:55 +0530

    palindrome

---
+++ palindrome.js
@@ -0,0 +1,19 @@
+//Eg: 121 is a palindrome number
+
+function isPalindrome(num) {
+    if(num < 0 ) return false
+    return num.toString() == num.toString().split("").reverse().join("")
+}
+
+// function isPalindrome(str) {
+//     for(let i = 0; i < str.length; i++) {
+//         if(str[i]!== str[str.length - 1 - i]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+console.log(isPalindrome(121)) // true
+console.log(isPalindrome(654)) // false
+//console.log(isPalindrome("racecar")) // true
